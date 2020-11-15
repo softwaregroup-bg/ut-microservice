8d045aa7d196fa7bc7c49b5eaa41a66e23c28226
Kalin Krustev <kalin.krustev@softwaregroup.com>
2020-11-15 04:52:17 +0200

    chore: simplify unit test config

--- test/server/unit.js
+++ test/server/unit.js
@@ -15,67 +15,13 @@ module.exports = {
             }
         }
     },
-    db: {
-        test: true,
-        updates: true,
-        connection: {
-            server: 'infradb14',
-            user: 'gitlab',
-            password: 'gitlab'
-        },
-        create: {
-            user: 'ut5'
-        }
-    },
+    db: true,
     utCache: {
         adapter: true
     },
-    utCore: {
-        adapter: true,
-        sqlStandard: true,
-        sqlTest: true
-    },
-    utCustomer: {
-        adapter: true,
-        orchestrator: true,
-        gateway: true,
-        test: true,
-        customer: {
-            debug: true
-        },
-        validationTest: true,
-        sql: {
-            ledger: true,
-            integration: true,
-            notice: true
-        },
-        sqlStandard: true,
-        sqlTest: true
-    },
-    utDocument: {
-        adapter: true,
-        sqlStandard: true,
-        sqlTest: true
-    },
-    utUser: {
-        adapter: true,
-        orchestrator: true,
-        gateway: true,
-        test: true,
-        identity: true,
-        permission: true,
-        validation: {
-            identity: true
-        },
-        validationTest: true,
-        sqlStandard: true,
-        sqlTest: true,
-        sql: {
-            hashParams: {
-                iterations: 1
-            },
-            debug: true
-        }
-    },
+    utCore: true,
+    utCustomer: true,
+    utDocument: true,
+    utUser: true,
     utMicroservice: true
 };
