ed47b94e34caf3390c9e1d8fb1e8dd89e3795361
Kalin Krustev <kalin.krustev@softwaregroup.com>
2021-08-17 10:35:26 +0300

    chore: file upload demo

--- portal/component/microservice.foo.demo.js
+++ portal/component/microservice.foo.demo.js
@@ -1,6 +1,7 @@
 // @ts-check
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { FileUpload } from 'ut-front-devextreme/core/prime';
 
 /** @type { import("../../handlers").handlerFactory } */
 export default ({
@@ -10,31 +11,39 @@ export default ({
         component$microserviceFooBrowse,
         component$microserviceFooOpen,
         component$microserviceFooNew,
-        component$microserviceFooDemo
+        component$microserviceFooDemo,
+        microserviceFooUpload
     },
     utMeta
 }) => ({
     'microservice.foo.demo': () => ({
         title: 'Foo demo',
         permission: 'microservice.foo.demo',
-        component: () => function fooDemo(params) {
-            // @ts-ignore
-            const pages = useSelector(({pages}) => pages);
-            return <div>
-                <div>TEST TAB</div><div>{JSON.stringify(params)}</div>
-                <button onClick={handle$microserviceFooClick}>
-                    Test reducer {JSON.stringify(pages)}
-                </button>
-                <button onClick={() => handleTabShow(component$microserviceFooBrowse, utMeta())}>
-                    Browse foo
-                </button>
-                <button onClick={() => handleTabShow(component$microserviceFooNew, utMeta())}>
-                    Create foo
-                </button>
-                <button onClick={() => handleTabShow([component$microserviceFooOpen, {id: 1}], utMeta())}>
-                    Open foo
-                </button>
-            </div>;
+        component: () => {
+            const handleClick = event => handle$microserviceFooClick(event, utMeta());
+            const handleBrowse = () => handleTabShow(component$microserviceFooBrowse, utMeta());
+            const handleCreate = () => handleTabShow(component$microserviceFooNew, utMeta());
+            const handleOpen = () => handleTabShow([component$microserviceFooOpen, { id: 1 }], utMeta());
+            const handleUpload = ({options, files}) => {
+                const formData = new window.FormData();
+                files.forEach(file => formData.append(options.props.name, file));
+                microserviceFooUpload({
+                    formData,
+                    ...options.props.url && {$http: {uri: options.props.url}}
+                }, utMeta());
+            };
+            return function fooDemo(params) {
+                // @ts-ignore
+                const pages = useSelector(({pages}) => pages);
+                return <div>
+                    <div>TEST TAB</div><div>{JSON.stringify(params)}</div>
+                    <button onClick={handleClick}>Test reducer {JSON.stringify(pages)}</button>
+                    <button onClick={handleBrowse}>Browse foo</button>
+                    <button onClick={handleCreate}>Create foo</button>
+                    <button onClick={handleOpen}>Open foo</button>
+                    <FileUpload name='file' url='/custom-upload-end-point' customUpload uploadHandler={handleUpload} multiple />
+                </div>;
+            };
         }
     })
 });
