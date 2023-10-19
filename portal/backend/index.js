c581e7cca0efd7beb822090fe2be9dfe6ad252a3
Kalin Krustev <kalin.krustev@gmail.com>
2023-10-19 10:41:28 +0300

    fix: do not mix cjs and esm

--- portal/backend/index.js
+++ portal/backend/index.js
@@ -1,7 +1,7 @@
 /** @type { import("../../handlers").handlerSet } */
-export default function backend() {
+module.exports = function backend() {
     return [
         () => ({namespace: ['microservice']}),
         require('./microservice.foo.process').default
     ];
-}
+};
